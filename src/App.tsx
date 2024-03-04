687c16c0c69407e40d3dff990d429ac98af7dc0f
Esteban Pastor <estebanandrespastor@gmail.com>
2024-03-04 16:31:11 -0300

    feat: :sparkles: Add funcionallity for add product to the table and save the data storage in the db and the products are sorted by last date/time

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import NavBar from "./components/ui/navbar/NavBar"
-import {Routes, Route} from "react-router-dom"
-import Home from "./pages/home/Home"
-import Products from "./pages/products/Products"
-
+import NavBar from "./components/ui/navbar/NavBar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import AddProduct from "./pages/add-product/AddProduct";
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <div className="wrapper">
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/products">
-          <Route index element={<Products />}/>
-          {/* <Route path="" element={}/> */}
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products">
+            <Route index element={<Products />} />
+            <Route path="add" element={<AddProduct />} />
+          </Route>
+        </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/add-product/AddProduct.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import "./add-products.scss";
+
+import { TextField, Button } from "@mui/material";
+import { IProduct } from "../../types/global.types";
+
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { baseURL } from "../../constants/url.constant";
+
+const AddProduct: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [product, setProduct] = useState<Partial<IProduct>>({
+    title: "",
+    brand: "",
+  });
+
+  const changeInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProduct({
+      ...product,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSaveButtonClick = () => {
+    if (product.title === "" || product.brand === "") {
+      alert("Please fill all the fields");
+      return;
+    }
+    const data: Partial<IProduct> = {
+      brand: product.brand,
+      title: product.title,
+    };
+    axios
+      .post(baseURL, data)
+      .then((response) => navigate("/products"))
+      .catch((error) => alert("Error while adding product"));
+  };
+
+  const handleBackButtonClick = () => {
+    navigate("/products");
+  };
+
+  return (
+    <div className="add-product">
+      <h2>Add New Product</h2>
+      <TextField
+        autoComplete="off"
+        label="Brand"
+        variant="outlined"
+        name="brand"
+        value={product.brand}
+        onChange={changeInputHandler}
+      />
+      <TextField
+        autoComplete="off"
+        label="Title"
+        variant="outlined"
+        name="title"
+        value={product.title}
+        onChange={changeInputHandler}
+      />
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSaveButtonClick}
+        >
+          Save
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleBackButtonClick}
+        >
+          Back
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default AddProduct;
